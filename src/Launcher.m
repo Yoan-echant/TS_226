cb2e8726d9315fdb31d8daae79bf2a30d17eb6f5
Guiguishop <71508215+Guiguishop@users.noreply.github.com>
2021-12-10 15:48:49 +0100

    test des courbes

--- src/Launcher.m
+++ src/Launcher.m
@@ -4,8 +4,11 @@ close all;
 N=10;
 u=randi([0,1],1,N);
 %u=[1 1 0 0];
-trellis=poly2trellis(3,[5,7]);
+%trellis=poly2trellis(3,[5,7]);
+%trellis = poly2trellis(7,[13,15]);
+trellis = poly2trellis(7,[133 171]); 
 s_i=0;
+%commcnv_plotoutputs(trellis.outputs, trellis.numOutputSymbols)
 closed=true;
 [c,s_f]=cc_encode(u,trellis,s_i,closed);
 
